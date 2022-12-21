8a299090c2f24d1fe70b489848bf31b1c024d736
Ishaan Bedi <bediishaan6@gmail.com>
2022-12-21 13:47:13 +0530

    Minor Changes to Welcome Text

--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,8 @@ const Home = () => {
         mvp link shortener. <code>ishn.xyz</code>
       </h1>
       <h3>
-        The purpose of this website and domain is to use a link shortener for{" "}
-        <Link target="_blank" href="https://www.ishaanbedi.in">
-          my own requirements.
-        </Link>
-        <br /> You can use this tool too to shorten your links using ishn.xyz as
-        the domain with a custom slug of your choice.
+        ishn.xyz is a minimal link shortener service that&apos;s easy and free
+        to use.
         <br /> Remember, you don&apos;t need to create an account to use this
         tool, as a result of which, you cannot delete/update links you create.
         <br />
