7f1ac439dc8f7ec0ebbd07757c8b3d9c8a17fa86
Ishaan Bedi <bediishaan6@gmail.com>
2022-12-21 14:52:47 +0530

    Fix for theme

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html color-mode="user">
+    <Html>
       <Head>
         <link rel="stylesheet" href="https://unpkg.com/mvp.css@1.12/mvp.css" />
       </Head>
