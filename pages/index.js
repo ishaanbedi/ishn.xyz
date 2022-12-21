5755f07de3a168ef3308e5719ad7312061418aa3
Ishaan Bedi <bediishaan6@gmail.com>
2022-12-21 14:05:16 +0530

    Minor changes to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,26 @@
+import Head from "next/head";
 import Link from "next/link";
 
 const Home = () => {
   return (
-    <main>
-      <h1>
-        mvp link shortener. <code>ishn.xyz</code>
-      </h1>
-      <h3>
-        ishn.xyz is a minimal link shortener service that&apos;s easy and free
-        to use.
-        <br /> Remember, you don&apos;t need to create an account to use this
-        tool, as a result of which, you cannot delete/update links you create.
-        <br />
-        Just go to <Link href="/create">/create</Link> to get started.
-      </h3>
-    </main>
+    <section>
+      <Head>
+        <title>ishn.xyz | Link Shortener</title>
+      </Head>
+      <main>
+        <h1>
+          minimal link shortener. <code>ishn.xyz</code>
+        </h1>
+        <h3>
+          ishn.xyz is a minimal link shortener service that&apos;s easy and free
+          to use.
+          <br /> Remember, you don&apos;t need to create an account to use this
+          tool, as a result of which, you cannot delete/update links you create.
+          <br />
+          Just go to <Link href="/create">/create</Link> to get started.
+        </h3>
+      </main>
+    </section>
   );
 };
 
