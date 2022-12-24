6aec0647a7cd604f1149d2c3c710994b64db0300
Ishaan Bedi <bediishaan6@gmail.com>
2022-12-24 09:21:37 +0530

    Installed Simpleanalytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,24 @@
-import { SessionProvider } from "next-auth/react"
-
+import { SessionProvider } from "next-auth/react";
+import Script from "next/script";
+import React from "react";
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  )
+    <React.Fragment>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+        <Script src="https://scripts.simpleanalyticscdn.com/latest.js" />
+        <noscript>
+          {/* eslint-disable @next/next/no-img-element */}
+          <img
+            src="https://queue.simpleanalyticscdn.com/noscript.gif"
+            alt=""
+            referrerPolicy="no-referrer-when-downgrade"
+          />
+        </noscript>
+      </SessionProvider>
+    </React.Fragment>
+  );
 }
