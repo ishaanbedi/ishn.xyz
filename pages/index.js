273a5f42f75d587fca24f6e4cf8490d12c089b2a
Ishaan Bedi <bediishaan6@gmail.com>
2022-12-26 23:03:20 +0530

    Fix bug Error JS-0092

--- pages/index.js
+++ pages/index.js
@@ -3,21 +3,19 @@ import NotLoggedIn from "../components/NotLoggedIn";
 import LoggedIn from "../components/LoggedIn";
 import Head from "next/head";
 const Home = () => {
-  const { data: session } = useSession();
-  while (session === undefined) {
-    return ( 
-      <header>
-        <main>
-          <Head>
-            <title>Loading...</title>
-          </Head>
-          <section>
-            <h1>Loading...</h1>
-          </section>
-        </main>
-      </header>
-    );
-  }
+  var { data: session } = useSession();
+  session === undefined && (
+    <header>
+      <main>
+        <Head>
+          <title>Loading...</title>
+        </Head>
+        <section>
+          <h1>Loading...</h1>
+        </section>
+      </main>
+    </header>
+  );
   if (session !== null && session !== undefined) {
     return <LoggedIn user={session.user} />;
   }
